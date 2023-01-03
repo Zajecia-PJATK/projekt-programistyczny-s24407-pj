cec904439cc911ba4ecc2daac647392bcfff19ea
s24407-pj <92219340+s24407-pj@users.noreply.github.com>
2023-01-03 23:54:16 +0100

    Update app.js

--- build/app.js
+++ build/app.js
@@ -1,4 +1,4 @@
 "use strict";
 var mongoClient = require('mongodb').MongoClient;
-var url = 'mongodb+srv://admin:heSADctvkcbVJJXd@database.czdcboz.mongodb.net/?retryWrites=true&w=majority';
+var url = 'mongodb+srv://admin:<password>@database.czdcboz.mongodb.net/?retryWrites=true&w=majority';
 var dbname = 'quizz-app';
