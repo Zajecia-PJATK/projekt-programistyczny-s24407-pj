71dc905079b235ae5e3a709e572c4b37c5192134
s24407-pj <s24407@pjwstk.edu.pl>
2023-01-03 23:16:11 +0100

    Added gitignore, node.js, typescript

---
+++ build/app.js
@@ -0,0 +1,4 @@
+"use strict";
+var mongoClient = require('mongodb').MongoClient;
+var url = 'mongodb+srv://admin:heSADctvkcbVJJXd@database.czdcboz.mongodb.net/?retryWrites=true&w=majority';
+var dbname = 'quizz-app';
---
+++ src/app.ts
@@ -0,0 +1,5 @@
+const mongoClient = require('mongodb').MongoClient
+
+const url = 'mongodb+srv://admin:heSADctvkcbVJJXd@database.czdcboz.mongodb.net/?retryWrites=true&w=majority'
+const dbname = 'quizz-app'
+
