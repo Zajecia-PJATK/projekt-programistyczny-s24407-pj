5a52a3715e8c10b9e7cb1efbf9e1875c49f4ab11
s24407-pj <s24407@pjwstk.edu.pl>
2023-01-16 23:30:30 +0100

    cut functions and put into files

---
+++ src/askForTitle.ts
@@ -0,0 +1,51 @@
+import { main } from "./app";
+
+export function askForTitle(): Promise<string> {
+    return new Promise((resolve, reject) => {
+      //remove all child elements of main
+      while (main.firstChild) {
+        main.removeChild(main.firstChild);
+      }
+      //Enter title of quiz
+      //create a <form> element
+      let form = document.createElement("form");
+      form.setAttribute("class", "fade-in");
+  
+      //create a <label> element
+      let label = document.createElement("label");
+      label.setAttribute("for", "title");
+      label.textContent = "Title:";
+  
+      //create an <input> element for the title
+      let titleInput = document.createElement("input");
+      titleInput.setAttribute("type", "text");
+      titleInput.setAttribute("id", "title");
+      titleInput.setAttribute("name", "title");
+      titleInput.setAttribute("required", "");
+  
+      //create a submit <input> element
+      let submitInput = document.createElement("input");
+      submitInput.setAttribute("type", "submit");
+  
+      //append the elements to the form
+      form.appendChild(label);
+      form.appendChild(titleInput);
+      form.appendChild(submitInput);
+  
+      //append the form to the main element
+      main.appendChild(form);
+  
+      //After submit title is equal to typed title
+      form = document.querySelector("form")!;
+      form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const title = document.querySelector<HTMLInputElement>("#title")!.value;
+        if (!title) {
+          reject("Title is required.");
+        } else {
+          resolve(title);
+        }
+      });
+    });
+  }
+  
