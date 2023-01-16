78598b0f1ef4db728691c2ce103fad4136647bd1
s24407-pj <s24407@pjwstk.edu.pl>
2023-01-16 19:56:14 +0100

    working creator

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,20 @@
 import { Question } from "./question";
 import { Quiz } from "./quiz";
 import { Type } from "./type";
-import { QuizCreator } from "./quizCreator";
+import { quizCreator } from "./quizCreator";
 
 
-export const main = document.querySelector("main")!;
-document.querySelector("h1")?.addEventListener("click",showQuizzes);
+export let main = document.querySelector("main")!;
 let quizStorage: Array<[Quiz]> = [];
 
 //render quizzes
 showQuizzes();
 
+
 //Shows quizzes to play on main page from quizStorage + add button
 function showQuizzes() {
     main.innerHTML = `<button class="rounded fade-in" id="add">+</button>`;
-    const button = document
-  .querySelector("#add")
-  ?.addEventListener("click", QuizCreator);
+    document.querySelector("h1")?.addEventListener("click",showQuizzes);
+    document.querySelector("#add")?.addEventListener("click", quizCreator);
+
 }
--- src/quizCreator.ts
+++ src/quizCreator.ts
@@ -2,67 +2,70 @@ import { main } from "./app";
 import { Quiz } from "./quiz";
 import { Settings } from "./settings";
 
-export function QuizCreator() {
-  let quiz: Quiz;
-  let title: string;
-
-  //Enter title of quiz
-  main.innerHTML = `
-  <form class="fade-in">
-      <label for="title">Title:</label>
-      <input type="text" id="title" name="title" required>
-      <input type="submit">
-  </form>`;
-
-  //After submit title is equal to typed title
-  let submit = document
-    .querySelector("input[type='submit']")
-    ?.addEventListener("click", () => {
-      title = document.querySelector<HTMLInputElement>("#title")!.value;
-    });
-
-  //Quiz settings
-  main.innerHTML = `
-    <form class="fade-in">
-    <fieldset>
-      <legend>Settings</legend>
-      <input type="checkbox" id="option1" name="option" />
-      <label for="option1">Negative points</label>
-      <br />
-      <input type="number" id="option2" name="option" min="0" max="600" value=0 />
-      <label for="option2">Time limit in s(0=unlimited)</label>
-      <br />
-      <input type="checkbox" id="option3" name="option"  />
-      <label for="option3">Randomise questions</label>
-      <br />
-      <input type="checkbox" id="option4" name="option"  />
-      <label for="option4">Randomise answers</label>
-      <br />
-      <input type="checkbox" id="option5" name="option"  />
-      <label for="option5">Disable previous questions</label>
-      <input type="submit">
-    </fieldset>
-    </form>
-    `;
-
-  //set settings
-  submit = document
-    .querySelector("input[type='submit']")
-    ?.addEventListener("click", () => {
-      let quiz = new Quiz(title, {
-        negativePoints: document.querySelector<HTMLInputElement>("#option1")?.checked,
-        time: +(document.querySelector<HTMLInputElement>("#option1")!.value),
-        randomQuestions: document.querySelector<HTMLInputElement>("#option1")?.checked,
-        randomAnswers: document.querySelector<HTMLInputElement>("#option1")?.checked,
-        prevQuestion: document.querySelector<HTMLInputElement>("#option1")?.checked,
-      });
-
-      //quiz.addQuestion;
-    });
-
-  //Enter settings
-
-  //Enter question
+export async function quizCreator() {
+  let quiz = new Quiz(await askForTitle(), await askForSettings());
 }
 
-function addQuestion() {}
+function askForTitle(): Promise<string> {
+  return new Promise((resolve, reject) => {
+    //Enter title of quiz
+    main.innerHTML = `<form class="fade-in">
+          <label for="title">Title:</label>
+          <input type="text" id="title" name="title" required>
+          <input type="submit">
+      </form>`;
+
+    //After submit title is equal to typed title
+    const submitButton = document.querySelector("input[type='submit']")!;
+    submitButton.addEventListener("click", () => {
+      const title = document.querySelector<HTMLInputElement>("#title")!.value;
+      resolve(title);
+    });
+  });
+}
+
+function askForSettings(): Promise<Settings> {
+  return new Promise((resolve, reject) => {
+    //Quiz settings
+    main.innerHTML = `
+        <form class="fade-in">
+        <fieldset>
+            <legend>Settings</legend>
+            <input type="checkbox" id="option1" name="option" />
+            <label for="option1">Negative points</label>
+            <br />
+            <input type="number" id="option2" name="option" min="0" max="600" value=0 />
+            <label for="option2">Time limit in s(0=unlimited)</label>
+            <br />
+            <input type="checkbox" id="option3" name="option"  />
+            <label for="option3">Randomise questions</label>
+            <br />
+            <input type="checkbox" id="option4" name="option"  />
+            <label for="option4">Randomise answers</label>
+            <br />
+            <input type="checkbox" id="option5" name="option"  />
+            <label for="option5">Disable previous questions</label>
+            <input type="submit">
+        </fieldset>
+        </form>
+        `;
+    //set settings
+    const submit = document
+      .querySelector("input[type='submit']")
+      ?.addEventListener("click", () => {
+        return {
+          negativePoints:
+            document.querySelector<HTMLInputElement>("#option1")?.checked,
+          time: +document.querySelector<HTMLInputElement>("#option2")!.value,
+          randomQuestions:
+            document.querySelector<HTMLInputElement>("#option3")?.checked,
+          randomAnswers:
+            document.querySelector<HTMLInputElement>("#option4")?.checked,
+          prevQuestion:
+            document.querySelector<HTMLInputElement>("#option5")?.checked,
+        };
+      });
+  });
+}
+
+function addQuestion(): void {}
