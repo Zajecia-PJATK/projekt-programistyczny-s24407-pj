2dc6def1987548b7a10f983e7f376ce3fb77c873
s24407-pj <s24407@pjwstk.edu.pl>
2023-01-18 23:51:44 +0100

    created board and inputs for names

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,45 @@
-import { Question } from "./question";
-import { Quiz } from "./quiz";
-import { showQuizzes } from "./showQuizzes";
-import { Type } from "./type";
+const main: HTMLElement = document.querySelector("main")!;
+const form: HTMLFormElement = document.querySelector("form")!;
+let rank: [name: string, winCount: number];
+const board = `<div id="board">
+    <button id="first" class="tile"></button>
+    <button id="second" class="tile"></button>
+    <button id="third" class="tile"></button>
+    <button id="fourth" class="tile"></button>
+    <button id="fifth" class="tile"></button>
+    <button id="sixth" class="tile"></button>
+    <button id="seventh" class="tile"></button>
+    <button id="eighth" class="tile"></button>
+    <button id="nineth" class="tile"></button>
+</div>`;
 
-export let main = document.querySelector("main")!;
-
-//create one test quiz
-const quiz = new Quiz("Football", {
-    //settings
-  negativePoints: false,
-  prevQuestion: false,
-  randomAnswers: false,
-  randomQuestions: false,
-  time: 0,
+form.addEventListener("submit", () => {
+  const inputFirst = <HTMLInputElement>document.querySelector("#first-player")!;
+  const inputSecond = <HTMLInputElement>(
+    document.querySelector("#second-player")!
+  );
+  const firstPlayer = inputFirst.value;
+  const secondPlayer = inputSecond.value;
+  console.log("Names added");
+  startGame(firstPlayer, secondPlayer);
 });
 
-//add question to quiz
-quiz.addQuestion(new Question(
-    "Who won World Cup 2022?",
-    Type.ONE,
-    [
-        ["Poland",false],
-        ["France",true],
-        ["Germany",false],
-        ["Qatar",false],
-    ]
-));
-localStorage.setItem(quiz.name,JSON.stringify(quiz));
+function startGame(firstPlayer: string, secondPlayer: string) {
+  // 1-X, 2-O
+  let currentPlayer = "X";
 
-//render quizzes
-showQuizzes();
+  //clearing main
+  main.innerHTML = "";
+
+  //add board to main
+  main.innerHTML = board;
+
+  const tiles = document.querySelectorAll(".tile");
+  tiles.forEach((tile) => {
+    tile.addEventListener("click", () => {
+      tile.setAttribute("disabled", "");
+      tile.textContent = currentPlayer;
+      currentPlayer = "O";
+    });
+  });
+}
--- src/settings.ts
+++
@@ -1,7 +0,0 @@
-export interface Settings {
-  negativePoints?: boolean; //Negative points for wrong answer
-  time?: number; //Time to answer for question, if 0 = false
-  randomQuestions?: boolean; //Random questions
-  randomAnswers?: boolean; //Random answers
-  prevQuestion?: boolean; //Cannot back to previous question
-}
